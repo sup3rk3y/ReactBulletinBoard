2c5c46a71d0081bd485c9fe9831eb776530777ce
sup3rk3y <lalosapost@gmail.com>
2019-04-18 01:52:18 +0200

    deploy new web page
    final version

---
+++ precache-manifest.c7ff4117fc6c2b9f2dc963364683031f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "abd30f6e0179f18f44c9",
+    "url": "/ReactBulletinBoard/static/js/runtime~main.67f43ac2.js"
+  },
+  {
+    "revision": "50f0457e56ee88fc441a",
+    "url": "/ReactBulletinBoard/static/js/main.0dbd8094.chunk.js"
+  },
+  {
+    "revision": "c90630e9cadd3cd1e2f1",
+    "url": "/ReactBulletinBoard/static/js/2.b4fad9df.chunk.js"
+  },
+  {
+    "revision": "50f0457e56ee88fc441a",
+    "url": "/ReactBulletinBoard/static/css/main.bd0d605a.chunk.css"
+  },
+  {
+    "revision": "77ff39a17062e22d63ce4a00c87eb380",
+    "url": "/ReactBulletinBoard/index.html"
+  }
+];
